c93c45bcea06a7c55e3cb6459a1a8092464d633a
Christoph Hoffmann <Christoph.Hoffmann@adesso.de>
2017-06-28 11:23:52 +0200

    Example db configuration

--- config/dev.exs
+++ config/dev.exs
@@ -48,4 +48,13 @@ config :logger, :console, format: "[$level] $message\n"
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
+# Configure your database
+#config :chankins, Chankins.Repo,
+#  adapter: Ecto.Adapters.Postgres,
+#  username: "postgres",
+#  password: "postgres",
+#  database: "chankins_dev",
+#  hostname: "localhost",
+#  pool_size: 10
+
 import_config "dev.secret.exs"
